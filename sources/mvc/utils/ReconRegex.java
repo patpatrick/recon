d61a03ccdb38d26c6093c8ce37217b8f4082625a
patpatrick <kicheandco@hotmail.fr>
2015-09-20 05:34:17 +0200

    [:)]Deleting bad ReconRegex and updating good ReconRegex.[(:]

--- sources/mvc/utils/ReconRegex.java
+++ sources/mvc/utils/ReconRegex.java
@@ -4,68 +4,58 @@ import java.utils.ArrayList ;
 
 public class ReconRegex {
 
-    public static String reconRegex(String filename){
-        ArrayList<String> result = this.simpleRecon(filename);
-        this.simplify(result);
+    private static void sizedMelt(ArrayList<String> result,int position,int size){
+        int cpt = 0 ;
+        while(cpt < size && this.melt(result,position)) 
+            cpt++;
     }
 
-    private static String simplify(ArrayList<String> result){
-        int size = 0 ;
-        int position = 0 ;
-
-        while( result.size() > 1 ){
-
-            this.reduce(result,position,size) ;
-
-        }
-
+    private static boolean melt(ArrayList<String> result,int position){
+        if( position+1 > result.size()-1 ) return false;
+        result.set(position,result.get(position)+result.get(position+1));
+        result.remove(position+1);
+        return true;
     }
 
-    private static boolean eat(ArrayList<String> result, int size, int position){
-        this.reduce(result,position+1,size) ;
-        if( result.get(position).matches( "^"+result.get(position+1)+"$" ) ){
-            if( result.get(position+1).matches( "\\(.*\\)\\+" ) )
-                result.remove(position+1) ;
-            else{
-                result.set(position, "("+result.get(position)+")+") ;
-                result.remove(position+1);
-            }
-           return true ; 
-        }else if( result.get(position).matches("\\("+result.get(position)+"\\)\\+") ){
-            result.remove(position+1) ;
+    private static boolean eat(ArrayList<String> result,int position,int size){
+        if( postion > result.size()-2 ) return false ;
+        if( result.get(position).matches("^"+result.get(position+1)+"$") ){
+            if( result.get(position).matches("\\("+result.get(position+1)+"\\)\\+") == false )
+                result.set(position,"("+result.get(position)+")+") ;
+            result.remove(position+1);
+            return true ;
+        }else if( result.get(position).matches("\\("+result.get(position+1)+"\\)\\+") ){
+            result.remove( position+1 );
             return true ;
         }
-        return false ;
+        return false;
     }
 
-    private static void reduce(ArrayList<String> result, int start, int size){
-        int cpt = start, max = start + size ;
-        while( cpt < result.size() && cpt < max ){
-            result.set(start,result.get(start)+result.get(start+1)) ;
-            result.remove(start+1);
+    private static int eatMore(ArrayList<String> result, int position, int size){
+        if( position > result.size()-1 ) return 0 ;
+        this.sizedMelt(result,position+1,size) ;
+
+        int cpt = 0 ;
+        while( eat(result,position,size) ){
             cpt++ ;
-        } 
-    }
-
-    private static ArrayList<String> simpleRecon(String filename){
-        String[] buffer = filename.split("") ;
-        ArrayList<String> result = new ArrayList<String>() ;
-        for( String c : buffer ){
-            result.add( this.reconChar( c ) ) ;
-            this.simpleReduce(result) ;
+            this.sizedMelt(result,position+1,size) ;
         }
-        return result ;
+
+        return cpt ; 
     }
 
-    private static void simplereduce(ArrayList<String> result){
-        int pos1 = result.size()-1 ;
-        int pos2 = result.size() - 2 ;
-
-        if( result.get(pos1).matches( "^"+result.get(pos2)+"$" ) ){
-            result.set( pos1, result.get(pos1)+"+" ) ;
-            result.remove( pos2 ) ;
-        }else if( result.get(pos1).matches( "^\\("+result.get(pos2)+"\\)\\+$") )
-            result.remove( pos2 ) ; 
+    private static String reduce(ArrayList<String> result){
+        int size = 0, position = 0 ;
+        while(result.size()>1){
+            position = 0 ;
+            this.sizedMelt(result,position,size);
+            while(position < result.size() ){
+                this.eatMore(result,position,size) ;
+                position++ ;
+            }
+            size++;
+        }
+        return result.get(0);
     }
 
     private static String reconChar( String c ){
@@ -78,6 +68,32 @@ public class ReconRegex {
         else if( c.matches("(\\?|\\^|\\|\\*|\\$|\\.|\\||\\$|\\[|\\]|\\(|\\))") )
             return "\\"+c ;
         return c ;
+    }
+
+    private static void simpleReduce(ArrayList<String> result){
+        int pos1 = result.size()-1 ;
+        int pos2 = result.size() - 2 ;
+
+        if( result.get(pos1).matches( "^"+result.get(pos2)+"$" ) ){
+            result.set( pos1, result.get(pos1)+"+" ) ;
+            result.remove( pos2 ) ;
+        }else if( result.get(pos1).matches( "^\\("+result.get(pos2)+"\\)\\+$") )
+            result.remove( pos2 ) ; 
+    }
+
+    public static String reconRegex(String filename){
+        ArrayList<String> result = this.simpleRecon(filename);
+        this.reduce(result); 
+    }
+
+    private static ArrayList<String> simpleRecon(String filename){
+        String[] buffer = filename.split("") ;
+        ArrayList<String> result = new ArrayList<String>() ;
+        for( String c : buffer ){
+            result.add( this.reconChar( c ) ) ;
+            this.simpleReduce(result) ;
+        }
+        return result ;
     }
 
 }
